a288a9690521477d356213fda5058bf8b94c23db
Rifyal Muhammad Imsan <rifyalmi@gmail.com>
2022-10-19 15:56:51 +0800

    update belajar listen

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log("5");
+const hello = document.querySelector(".hello:first-child h1")
+
+console.log(hello)
+hello.style.color = "tomato"
+
+function titleOnClick(){
+    console.log("halo bang")
+}
+
+function titleOnHover(){
+    //hello.setAttribute("style", "color: magenta")
+    hello.style.color = "magenta"
+}
+
+function titleOffHover(){
+    //hello.setAttribute("style", "color: tomato")
+    hello.style.color = "tomato"
+}
+
+hello.addEventListener("mouseover", titleOnHover)
+hello.addEventListener("mouseout", titleOffHover)
+hello.addEventListener("click", titleOnClick)
